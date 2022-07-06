4f27c968e0575fe2ed3354bba287fb9e374b7192
Shota Yamahira <shyamahira@bbtower.co.jp>
2022-07-06 12:02:30 +0900

    docs: Document: Extending documentation - JSDocは表示されない問題発生中

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -12,6 +12,10 @@ export default {
       },
       options: ['tiny', 'small', 'medium', 'large']
     }
+  },
+  parameters: {
+    componentSubtitle:
+      'Displays an image that represents a user or organization'
   }
 };
 
@@ -33,6 +37,12 @@ export const Sizes = (args) => (
 Sizes.args = {
   username: 'Tom Coleman',
   src: 'https://avatars2.githubusercontent.com/u/132554'
+};
+Sizes.parameters = {
+  docs: {
+    // The story now contains a description
+    storyDescription: '4 sizes are supported.'
+  }
 };
 
 export const Initials = (args) => (
